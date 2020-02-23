5aa608b36062796652f7f3320579e0da9e228bdb
Ashish Dutt <ashish.dutt8@gmail.com>
2020-02-23 09:25:20 +0800

    initial commit

---
+++ scripts/basics/convert_dict_to_pandas_dataframe.py
@@ -0,0 +1,39 @@
+# Objective: create a list and convert it to a pandas data frame
+# script create date: 23/Feb/2020
+
+# load library
+import pandas as pd
+# creat a dictionary
+mydict = {'name':'ashish',
+          'age':28,
+          'salary':20000
+          }
+mydict
+
+# Append/add new items to a dictionary
+
+# Method #1: Create multiple lists.
+name_list = ['ashish','alex','bob']
+age_list = [15,29,37]
+salary_list = [10000,2987,3456]
+# Convert multiple lists into a dictionary object
+mydict = {'name':[],'age':[],'salary':[]}
+mydict['name'].append(name_list)
+mydict['age'].append(age_list)
+mydict['salary'].append(salary_list)
+# print the dictionary
+print(mydict)
+# convert dictionary to pandas dataframe
+df= pd.DataFrame(mydict)
+print(df)
+
+# Method #2: Simple and elegant  
+# Create a data frame and within it create a dictionary. Within the dictionary, create key name as column name
+# and put (multiple key values as a list
+df = pd.DataFrame(data={'name':['ashish','alex','sandy'],
+                        'age':[18,23,34],
+                        'salary':[1876,2786,3478]
+                        }
+                  )
+print(df)
+
