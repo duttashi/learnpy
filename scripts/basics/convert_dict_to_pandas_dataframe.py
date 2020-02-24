015e712c0613394ced4c71b765dad04b08559bc1
Ashish Dutt <ashish.dutt8@gmail.com>
2020-02-24 11:22:41 +0800

    file modified

--- scripts/basics/convert_dict_to_pandas_dataframe.py
+++ scripts/basics/convert_dict_to_pandas_dataframe.py
@@ -3,6 +3,7 @@
 
 # load library
 import pandas as pd
+
 # creat a dictionary
 mydict = {'name':'ashish',
           'age':28,
@@ -10,9 +11,8 @@ mydict = {'name':'ashish',
           }
 mydict
 
-# Append/add new items to a dictionary
-
 # Method #1: Create multiple lists.
+# Append/add new items to a dictionary
 name_list = ['ashish','alex','bob']
 age_list = [15,29,37]
 salary_list = [10000,2987,3456]
@@ -21,13 +21,13 @@ mydict = {'name':[],'age':[],'salary':[]}
 mydict['name'].append(name_list)
 mydict['age'].append(age_list)
 mydict['salary'].append(salary_list)
-# print the dictionary
-print(mydict)
+
 # convert dictionary to pandas dataframe
 df= pd.DataFrame(mydict)
+print("\n Method # 1")
 print(df)
 
-# Method #2: Simple and elegant  
+# Method #2: Simple and elegant
 # Create a data frame and within it create a dictionary. Within the dictionary, create key name as column name
 # and put (multiple key values as a list
 df = pd.DataFrame(data={'name':['ashish','alex','sandy'],
@@ -35,5 +35,15 @@ df = pd.DataFrame(data={'name':['ashish','alex','sandy'],
                         'salary':[1876,2786,3478]
                         }
                   )
+print("\n Method # 2")
+print(df)
+
+# Method #3: Create dataframe using existing list objects
+df = pd.DataFrame(data={'name': name_list,
+                        'age': age_list,
+                        'salary': salary_list
+                        }
+                  )
+print("\n Method # 3")
 print(df)
 
