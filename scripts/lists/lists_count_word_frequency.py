44370b22f47003ab6703660b6794db270703e5c4
ashish <ashish.dutt8@gmail.com>
2020-04-06 13:01:29 +0800

    initial commit

--- scripts/lists/lists_count_word_frequency.py
+++ scripts/lists/lists_count_word_frequency.py
@@ -1,5 +1,27 @@
-
 # objective: Read a text file, and count the frequency of words
 
-# read the text file
-txtFile = open("C:\Users\Ashoo\Documents\PythonPlayground\learnpy\data\alice.txt").readlines()
+# load required libraries
+import os, re
+
+# set relative path location
+dataPath = os.path.dirname(__file__)
+relPath = "C:/Users/Ashoo/Documents/PythonPlayground/learnpy/data/alice.txt"
+abs_file_path = os.path.join(dataPath, relPath)
+
+# open the text file from relative path
+txtFile = open(abs_file_path, 'r')
+# Read text file to list
+# python's file.readlines() method returns a list of the lines in the file:
+txtData = txtFile.readlines()
+# read text file and show its length
+print("File length:",len(txtData))
+# print text data
+print(txtData)
+# do basic text cleaning
+
+
+
+
+# close file connection
+txtFile.close()
+
