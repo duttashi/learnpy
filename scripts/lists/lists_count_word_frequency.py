a8846b8bc5ece1a60e43df42d5298b35e95230c0
ashish <ashish.dutt8@gmail.com>
2020-04-06 13:01:21 +0800

    initial commit

---
+++ scripts/lists/lists_count_word_frequency.py
@@ -0,0 +1,5 @@
+
+# objective: Read a text file, and count the frequency of words
+
+# read the text file
+txtFile = open("C:\Users\Ashoo\Documents\PythonPlayground\learnpy\data\alice.txt").readlines()
