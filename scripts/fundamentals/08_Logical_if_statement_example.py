b88d64281cee6abd347902e217c5cf5191328ec1
duttashi <ashish.dutt8@gmail.com>
2020-07-15 15:15:53 +0800

    modified code - enhanced code by enclosing it in a while loop

--- scripts/fundamentals/08_Logical_if_statement_example.py
+++ scripts/fundamentals/08_Logical_if_statement_example.py
@@ -12,22 +12,32 @@ Greater than or equal to 30, classification- Obesity
 
 Formula BMI = weight/(height*height)
 """
-print("Body Mass Index Calculator")
-print("---------------------------")
-weight = float(input("Enter your current weight in Kg (e.g. 75.5): "))
-height = float(input("Enter your height in feet (e.g. 1.70): "))
-bmi = round( (weight/(height**2)), 2)
-if(bmi<=18.5):
-    classification = "Underweight"
-elif(bmi>18.5 and bmi<=24.9):
-    classification = "Normal weight"
-elif(bmi>24.9 and bmi<=29.9):
-    classification = "Over weight"
-elif(bmi>=30):
-    classification = "Obese"
-else:
-    classification ="Invalid"
-print("Your current BMI is: %.2f" % bmi, "and your",classification)
+user_choice = "y"
+
+while(user_choice!="n"):
+
+    print("Body Mass Index Calculator")
+    print("---------------------------")
+
+    weight = float(input("Enter your current weight in Kg (e.g. 75.5): "))
+    height = float(input("Enter your height in feet (e.g. 1.70): "))
+    bmi = round( (weight/(height**2)), 2)
+
+    if(bmi<=18.5):
+        classification = "Underweight"
+    elif(bmi>18.5 and bmi<=24.9):
+        classification = "Normal weight"
+    elif(bmi>24.9 and bmi<=29.9):
+        classification = "Over weight"
+    elif(bmi>=30):
+        classification = "Obese"
+    else:
+        classification ="Invalid"
+    print("Your current BMI is: %.2f" % bmi, "and your",classification)
+
+    user_choice = input("Do you want to check BMI again (y/n):")
+
+    
 
 
 
