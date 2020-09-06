af9283650615db9a5720c241d95feba74aa2bbe2
Ashish Dutt <ashish.dutt8@gmail.com>
2020-09-06 10:59:53 +0800

    initial commit

---
+++ scripts/basics/loop_for_example.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 25 21:06:20 2020
+
+@author: Ashish
+For and while loops
+"""
+
+# declare a list of items
+subjects = ['math','science','arts','social-science']
+
+for i in range(len(subjects)):
+    # prints the length of each string in the list
+    print(len(subjects[i]))
