9a6d14e0bdcf3f3e2fbdb599b8dca8b0fe9465ec
Ashish Dutt <ashish.dutt8@gmail.com>
2020-09-06 11:00:12 +0800

    initial commit

---
+++ scripts/basics/set_example.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 25 21:44:32 2020
+
+set examples
+@author: Ashish
+"""
+
+# first create a list
+student_grades = ['A','A','B','B','C']
+# now use the set() method
+print(student_grades)
+student_grades = set(student_grades)
+print(student_grades)
+
